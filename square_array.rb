6908bd4c2fc9a525c7f037cd51365eed4bec08d5
Eliot Howes <ejhowes@gmail.com>
2019-05-23 22:20:53 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,6 +1,6 @@
 def square_array(array)
   # your code here
   new_array = []
-  array.each {|number| new_array.push(Math.sqrt(number))}
+  array.each {|number| new_array.push(number ** 2)}
   return new_array
 end
